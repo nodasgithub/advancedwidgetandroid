d62bf55fdc33f0716257635c189caa0ca2695a4d
nodasgithub <47042341+nodasgithub@users.noreply.github.com>
2020-11-09 00:54:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/advancedwidgetandroid/NewAppWidget.java
@@ -0,0 +1,44 @@
+package com.example.advancedwidgetandroid;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        final int numberOfWidgets = appWidgetIds.length;
+
+        for (int i = 0; i < numberOfWidgets; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+
+            Intent intent = new Intent(context, NewsService.class);
+            intent.putExtra("appWidgetId", appWidgetId);
+            context.startService(intent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+
+        }
+
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
